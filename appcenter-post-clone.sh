05a4ae757bad6af3bd48275ce0aa5bbd4ba89385
David Wu <ji3g4kami@gmail.com>
2020-03-30 21:17:26 +0100

    [fix] appcenter post script2

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-pod repo update
+echo "[script log] uninstalling all cocoapods versions"
+sudo gem uninstall cocoapods --all
+echo "[script log] installing cocoapods version 1.8.4"
+sudo gem install cocoapods -v 1.8.4
+
+echo "[script log] pod install..."
 pod install
